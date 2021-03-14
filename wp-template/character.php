031c9dee46010e9f25ae6a994a884748229af688
Ansel Robateau <ansel@casechek.com>
2021-03-14 14:21:47 -0500

    style: 💄 customize site colors (#3)
    * style: 💄 customize site colors
    
    * feat: 🎸 Add palate 1
    
    * feat: 🎸 Lighten the background button row color
    
    * feat: 🎸 Set palete 2
    
    * feat: 🎸 Set button text to white
    
    * feat: 🎸 Simplify template
    
    * feat: 🎸 Lighten page background color
    
    * feat: 🎸 Customize the table border
    
    * style: 💄 Set the border and header colors
    
    * feat: 🎸 Set the post-title color
    
    * feat: 🎸 Set the horizontal line to a solid color

---
+++ wp-template/character.php
@@ -0,0 +1,83 @@
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body {font-family: Arial;}
+
+/* Style the tab */
+.tab {
+  overflow: hidden;
+  border: 1px solid #0b280c;
+  background-color: #37662a;
+}
+
+/* Style the buttons inside the tab */
+.tab button {
+  background-color: inherit;
+  float: left;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  padding: 4px 6px;
+  transition: 0.3s;
+  font-size: 16px;
+  color: white;
+  border-radius: 0px;
+}
+
+/* Change background color of buttons on hover */
+.tab button:hover {
+  background-color: #194515;
+}
+
+/* Create an active/current tablink class */
+.tab button.active {
+  background-color: #194515;
+}
+
+/* Style the tab content */
+.tabcontent {
+  display: none;
+  padding: 6px 12px;
+  border: 1px solid #0b280c;
+  border-top: none;
+}
+</style>
+
+
+<div style="">
+<div class="tab" style="color: black">
+  <button class="tablinks active" onclick="openCity(event, 'Mae')">Mae</button>
+  <button class="tablinks" onclick="openCity(event, 'Back')">Back</button>
+  <button class="tablinks" onclick="openCity(event, 'Front')">Front</button>
+</div>
+
+<div id="Mae" class="tabcontent">
+  <img width="100%" src="https://maeandthekingdomsofseasons.com/wp-content/uploads/2021/02/Alex.png">
+</div>
+
+<div id="Back" class="tabcontent">
+  <h3>Back</h3>
+  <p>This is the back.</p> 
+</div>
+
+<div id="Front" class="tabcontent">
+  <h3>Front</h3>
+  <p>This is the front.</p>
+</div>
+
+</div>
+<script>
+function openCity(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+document.getElementById("Mae").style.display = "block";
+</script>
