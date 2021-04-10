70fe2ad14c027f673d81ef9d33ce03f158c0fa95
Ansel Robateau <ansel@casechek.com>
2021-04-10 11:40:05 -0500

    feat: 🎸 Create a sample plugin

---
+++ wp-content/plugins/awesomeplugin/awesomeplugin.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Awesomeness Creator
+Plugin URI: http://my-awesomeness-emporium.com/
+description: >-
+a plugin to create awesomeness and spread joy
+Version: 1.0
+Author: Mr. Awesome
+Author URI: http://mrtotallyawesome.com
+License: GPL2
+*/
+?>
