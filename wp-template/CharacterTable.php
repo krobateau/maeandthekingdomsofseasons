aad3824e9c68034dbf4fe8d87f34c8d078fce46d
Ansel Robateau <ansel@casechek.com>
2021-05-24 22:20:35 -0500

    feat: 🎸 Support the name parameter

--- wp-template/CharacterTable.php
+++ wp-template/CharacterTable.php
@@ -2,7 +2,7 @@
 /*
 Template Name: Character Table
 */
-
-$characterData = file_get_contents('https://script.google.com/macros/s/AKfycbwk4hdhsehHOSd_zUdZ2hXvNsTU0TNNQbKM3eX7vwGYhDixptc0/exec');
+$characterName = $_GET["name"];
+$characterData = file_get_contents('https://script.google.com/macros/s/AKfycbwk4hdhsehHOSd_zUdZ2hXvNsTU0TNNQbKM3eX7vwGYhDixptc0/exec?name=' . $characterName);
 echo "<code>$characterData</code>";
 ?>
