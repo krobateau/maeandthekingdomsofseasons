8323bd95686af6f7c1c68e595c29b6698d81ade4
Ansel Robateau <ansel@casechek.com>
2021-06-07 20:46:24 -0500

    feat: 🎸 Move javascript to global.js

--- wp-template/CharacterTable.php
+++ wp-template/CharacterTable.php
@@ -86,22 +86,6 @@ body {font-family: Arial;}
   </div>
 
 </div>
-<script>
-function openPose(evt, cityName) {
-  var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-  document.getElementById(cityName).style.display = "block";
-  evt.currentTarget.className += " active";
-}
-document.getElementById("Mae").style.display = "block";
-</script>
 <figure class="wp-block-table m0 is-style-regular">
   <table>
     <tbody>
--- wp-template/assets/js/global.js
+++ wp-template/assets/js/global.js
@@ -191,3 +191,17 @@ jQuery(document).ready(function($) {
 		item.fetch(characterName);
 	}
 });
+
+function openPose(evt, poseName) {
+	var i, tabcontent, tablinks;
+	tabcontent = document.getElementsByClassName("tabcontent");
+	for (i = 0; i < tabcontent.length; i++) {
+		tabcontent[i].style.display = "none";
+	}
+	tablinks = document.getElementsByClassName("tablinks");
+	for (i = 0; i < tablinks.length; i++) {
+		tablinks[i].className = tablinks[i].className.replace(" active", "");
+	}
+	document.getElementById(poseName).style.display = "block";
+	evt.currentTarget.className += " active";
+}
